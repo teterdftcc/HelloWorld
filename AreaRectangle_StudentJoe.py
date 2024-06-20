111a443602b2f43f7d1237210a2395123a820193
teterdftcc <31602884+teterdftcc@users.noreply.github.com>
2024-06-20 13:38:53 -0400

    Add files via upload

---
+++ AreaRectangle_StudentJoe.py
@@ -0,0 +1,26 @@
+# Program calculates the Area of a Rectangle.
+# 06/20/2024
+# AreaRectangle
+# Joe Student
+#
+# ******************* Pseudocode *******************
+# Ask user "Please Enter Rectangle Length: "
+# Display "------------------------------------------------"
+# Input length
+# Ask user "Please Enter Rectangle Width: "
+# Input width
+# Set area = length * width
+# Display "Length entered:", length
+# Display "Width entered:", width
+# Display "------------------------------------------------"
+# Display "The Area of your Rectangle is:", area
+
+print('This program calculates the Area of a Rectangle')
+print('------------------------------------------------')
+length = int(input('Please Enter Rectangle Length: '))
+width = int(input('Please Enter Rectangle Width: '))
+Area = length * width
+print("Length entered:", length)
+print("Width entered:", width)
+print('------------------------------------------------')
+print("The Area of the Rectangle is:", area)
